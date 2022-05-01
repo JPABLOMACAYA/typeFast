20b8423c103508fd83fb7acabc082614b318b16b
JPABLOMACAYA <103909266+JPABLOMACAYA@users.noreply.github.com>
2022-05-01 17:37:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -42,12 +42,11 @@ function addToDom () {
 
 addToDom ();
 
-
 var tiempoMostrado = document.querySelector("#timeSpan");
 
 var palabraIngresada = document.querySelector("#text");
 
-tiempoMostrado.innerHTML = time + "s";  //me lo sugirió VS en reemplazo de --> time + "s"
+tiempoMostrado.innerHTML = time + "s";
 
 //A continuación se compara el value del input con el inner del título que muestra la palabra aleatoria
 palabraIngresada.addEventListener("keypress", function (e) { 
@@ -97,43 +96,3 @@ function gameOver () {
     contenedorGameOver.innerHTML = tituloSinTiempo + parrafoPuntajeFinal + botonVolveAEmpezar;
     divisionPrincipal.innerHTML = ""
 }
-
-/*
-
-function actualizarTiempo () {   //Otra manera de solucionar la cuenta atrás, no necesita de clearInterval
-    if (time > 0) {
-    time --;
-    tiempoMostrado.innerHTML = time + "s";
-    }
-}
-
-
-var tiempoMostrado = document.querySelector("#timeSpan")
-
-tiempoMostrado.textContent = `${time}s`  //me lo sugirió VS en reemplazo de --> time + "s"
-
-var inputTexto = document.querySelector("#text")
-
-var palabraIngresada = document.getElementById("text").value
-
-inputTexto.addEventListener("keypress", function (e) {
-    console.log(e);
-    if (eventkey == "Enter") {if (palabraIngresada == palabraAleatoria) {
-        (time += 3);
-        (e = "");
-        addToDom(); //verificar invocación de la función addToDom();
-    } else {addToDom(); //solo invoca a la función addToDOm()
-    } }
-})
-
-
-function actualizarTiempo () {
-    if (time === 0) {
-        clearInterval(timeInterval)
-    } else {time --}
-}
-
-let timeInterval = setInterval (actualizarTiempo, 1000);
-
-var parrafoPuntajeFinal = `<p>TU PUNTAJE FINAL ES DE: ${score}</p>`
-*/
